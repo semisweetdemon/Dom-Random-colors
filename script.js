636e0905b7f78138a2b5895179865e53eb5f8400
semisweetdemon <semisweetdemon@gmail.com>
2023-12-23 11:21:53 +0600

    true svetofor

--- script.js
+++ script.js
@@ -65,17 +65,68 @@
 // });
 
 const circle = document.querySelectorAll('.circle');
+const round1 = document.querySelector('.round1');
+const round2 = document.querySelector('.round2');
+const round3 = document.querySelector('.round3');
 
-let color = 'green';
+// let color = 'green';
 
-setInterval(function () {
-	color = color === 'red' ? 'green' : 'red';
-}, 4000);
+// setInterval(function () {
+// 	color = color === 'red' ? 'green' : 'red';
+// }, 4000);
+
+// setInterval(() => {
+// 	circle.forEach((el) => (el.style.background = ''));
+// }, 2000);
+
+// setInterval(() => {
+// 	circle.forEach((el) => (el.style.background = color));
+// }, 4000);
+
+let color = 'red';
 
 setInterval(() => {
-	circle.forEach((el) => (el.style.background = ''));
+	color = color === 'red' ? 'green' : color === 'green' ? 'yellow' : 'red';
+	if (color === 'red') {
+		round1.style.background = color;
+		round2.style.background = '';
+		round3.style.background = '';
+	} else if (color === 'yellow') {
+		round1.style.background = '';
+		round2.style.background = color;
+		round3.style.background = '';
+	} else {
+		round1.style.background = '';
+		round2.style.background = '';
+		round3.style.background = color;
+	}
 }, 2000);
 
-setInterval(() => {
-	circle.forEach((el) => (el.style.background = color));
-}, 4000);
+// const round1 = document.querySelector('.round1');
+// const round2 = document.querySelector('.round2');
+// const round3 = document.querySelector('.round3');
+
+// const circle = document.querySelectorAll('.circle');
+
+// let color1 = 'green';
+// let color2 = 'yellow';
+// let color3 = 'red';
+
+// let n = color3;
+
+// setInterval(() => {
+// 	n = n === color1 ? color2 : n === color2 ? color3 : color1;
+// 	if (n === color1) {
+// 		round1.style.background = n;
+// 		round2.style.background = '';
+// 		round3.style.background = '';
+// 	} else if (n === color2) {
+// 		round1.style.background = '';
+// 		round2.style.background = n;
+// 		round3.style.background = '';
+// 	} else {
+// 		round1.style.background = '';
+// 		round2.style.background = '';
+// 		round3.style.background = n;
+// 	}
+// }, 2000);
---
+++ slider/scipt.js
@@ -0,0 +1,44 @@
+//? Consts
+
+// Slider images
+const sliderImages = document.querySelector('.slider__images');
+const sliderOne = document.querySelector('.slider__one');
+const sliderTwo = document.querySelector('.slider__two');
+const sliderThree = document.querySelector('.slider__three');
+
+// let n = 3;
+
+// let one = 'url(/images/krystian-tambur-k7rZ8wTfABA-unsplash.jpg) no-repeat center/ cover';
+// let two = 'url(/images/jong-marshes-79mNMAvSORg-unsplash.jpg) no-repeat center/ cover';
+// let three = 'url(/images/yang-wewe-uiQrQ19xn-U-unsplash.jpg) no-repeat center/ cover';
+
+// setInterval(() => {
+// 	n = n === 3 ? 1 : n === 1 ? 2 : 3;
+// 	sliderImages.style.background = n === 1 ? one : n === 2 ? two : three;
+// }, 1000);
+
+// setInterval(() => {
+// 	n = n === 3 ? 1 : n === 1 ? 2 : 3;
+// 	if (n === 1) {
+// 		sliderOne.style.display = 'block';
+// 		sliderOne.style.transform = 'scale(1)';
+// 		sliderTwo.style.display = '';
+// 		sliderTwo.style.transform = '';
+// 		sliderThree.style.display = '';
+// 		sliderThree.style.transform = '';
+// 	} else if (n === 2) {
+// 		sliderOne.style.display = '';
+// 		sliderOne.style.transform = '';
+// 		sliderTwo.style.display = 'block';
+// 		sliderTwo.style.transform = 'scale(1)';
+// 		sliderThree.style.display = '';
+// 		sliderThree.style.transform = '';
+// 	} else {
+// 		sliderOne.style.display = '';
+// 		sliderOne.style.transform = '';
+// 		sliderTwo.style.display = '';
+// 		sliderTwo.style.transform = '';
+// 		sliderThree.style.display = 'block';
+// 		sliderThree.style.transform = 'scale(1)';
+// 	}
+// }, 1000);
